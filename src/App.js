e01295cd6f04e149f2288b0d2be4827e8c39a230
anit-pro01 <anku.emotiondigital@gmail.com>
2020-03-01 01:32:21 +0530

    Route change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Layout from './Components/Layout/Layout';
-import { HashRouter, Route, Redirect} from "react-router-dom";
+import { HashRouter, Route, Switch, } from "react-router-dom";
 import Main from './Containers/TableVeiw/Main/Main';
 import DetailVeiw from "./Containers/DetailVeiw/DetailVeiw"
 
@@ -10,9 +10,11 @@ function App() {
     <HashRouter>
         <div className="App">
           <Layout>
+            <Switch>
                 <Route path ="/" exact component = {Main} />
                 <Route path = "/user/:userid"  exact component = {DetailVeiw} />
                 <Route render = {() => <h2>404 ! Page not Found!!!</h2>} />
+            </Switch>
           </Layout>
       </div>
     </HashRouter>
