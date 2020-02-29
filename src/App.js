6100d9f375d715c7b28f2fc78d5b9d1388dc445c
anit-pro01 <anku.emotiondigital@gmail.com>
2020-02-29 23:41:34 +0530

    chanes in package

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
 import Layout from './Components/Layout/Layout';
-import { BrowserRouter, Route} from "react-router-dom";
+import { HashRouter, Route} from "react-router-dom";
 import Main from './Containers/TableVeiw/Main/Main';
 import DetailVeiw from "./Containers/DetailVeiw/DetailVeiw"
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <div className="App">
           <Layout>
                 <Route path ="/" exact component = {Main} />
                 <Route path = "/users/:userid"  exact component = {DetailVeiw} />
           </Layout>
       </div>
-    </BrowserRouter>
+    </HashRouter>
     
   );
 }
