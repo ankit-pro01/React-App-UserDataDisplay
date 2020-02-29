7da047a434e019d2ae9856e85b67a93e161cd4b3
anit-pro01 <anku.emotiondigital@gmail.com>
2020-02-29 22:34:26 +0530

    Updates

---
+++ precache-manifest.88aa343842afebf2c073f769e5dbd075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2475d79634723c9d8b7d8fd9038771a9",
+    "url": "/React-App-UserDataDisplay/index.html"
+  },
+  {
+    "revision": "9cb3bd52f1350c20c015",
+    "url": "/React-App-UserDataDisplay/static/css/main.a101f095.chunk.css"
+  },
+  {
+    "revision": "f00d3285727e72f24451",
+    "url": "/React-App-UserDataDisplay/static/js/2.3b147bf9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/React-App-UserDataDisplay/static/js/2.3b147bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb3bd52f1350c20c015",
+    "url": "/React-App-UserDataDisplay/static/js/main.8125addc.chunk.js"
+  },
+  {
+    "revision": "9345a8ca273cb7bbca2a",
+    "url": "/React-App-UserDataDisplay/static/js/runtime-main.66614c99.js"
+  }
+]);
